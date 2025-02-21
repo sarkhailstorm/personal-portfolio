94086e7fb10fe90f92698bfc229df47f08b4c836
Sarkhail <sarkhailabbas@gmail.com>
2025-02-21 08:07:46 +0530

    Add react icons.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { IoHome } from "react-icons/io5";
+import { IoPerson } from "react-icons/io5";
+import { FaTools } from "react-icons/fa";
+import { FaFolderOpen } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="text-3xl">
+      <Link href="/">
+        <IoHome />
+      </Link>
+      <Link href="/services">
+        <IoPerson />
+      </Link>
+      <Link href="/skills">
+        <FaTools />
+      </Link>
+      <Link href="/projects">
+        <FaFolderOpen />
+      </Link>
+      <Link href="/contact">
+        <FaEnvelope />
+      </Link>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
