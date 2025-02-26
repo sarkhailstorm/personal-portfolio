3022dc4bcaeba3003a53440fe74db16e9341375e
Sarkhail <sarkhailabbas@gmail.com>
2025-02-26 11:45:28 +0530

    Add project images.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,27 +9,27 @@ const data = [
     desc: "This is a beautiful web app that will blow your head.",
   },
   {
-    img: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
-    title: "3D Portfolio Website",
+    img: "Weather App.png",
+    title: "Weather App",
+    desc: "This is a beautiful web app that will blow your head.",
+  },
+  {
+    img: "Cloud File Manager.png",
+    title: "Cloud File Manager",
     desc: "This is a beautiful web app that will blow your head.",
   },
   {
     img: "3D Portfolio.png",
-    title: "Smart Home App",
+    title: "3D Portfolio Website",
     desc: "This is a beautiful web app that will blow your head.",
   },
   {
-    img: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
-    title: "Weather Application",
-    desc: "This is a beautiful web app that will blow your head.",
-  },
-  {
-    img: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+    img: "Task Management App.png",
     title: "Task Management App",
     desc: "This is a beautiful web app that will blow your head.",
   },
   {
-    img: "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+    img: "Google Calculator.png",
     title: "Google Calculator",
     desc: "This is a beautiful web app that will blow your head.",
   },
@@ -37,13 +37,13 @@ const data = [
 
 const ProjectsPage = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8 overflow-x-hidden">
+    <div className="h-screen flex flex-col items-center justify-center gap-12 overflow-x-hidden">
       <div>
         <h1 className="text-4xl font-bold tracking-wider">My Work</h1>
       </div>
       <div className="flex gap-3 overflow-y-scroll flex-wrap justify-center items-center h-[345px]">
         {data.map((info, index) => (
-          <motion.div key={index} className="card bg-base-100 w-80 border-2 border-gray-800 shadow-xl">
+          <motion.div key={index} className="card bg-[#1b2838] w-80 border-2 border-gray-800 shadow-xl">
             <figure>
               <img className=""
                 src={info.img}
