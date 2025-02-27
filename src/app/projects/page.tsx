a4d5806f3abeb3826c23935c0dc1c9914499f34e
Sarkhail <sarkhailabbas@gmail.com>
2025-02-27 17:17:52 +0530

    Add group-hover effect to cards.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -61,18 +61,27 @@ const ProjectsPage = () => {
         {projects.map((project, index) => (
           <motion.div
             key={index}
-            className="card bg-[#0f111a] w-80 border-[1.5px] border-x-indigo-700 border-y-violet-800 shadow-xl"
+            className="relative group card bg-[#0f111a] w-80 border-[1.5px] border-x-indigo-700 border-y-violet-800 shadow-xl overflow-hidden cursor-pointer"
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 0.5 }}
             viewport={{ once: true }}
           >
             <figure>
-              <img src={project.img} alt={`Image ${index}`} />
+              <img
+                src={project.img}
+                alt={`Image ${index}`}
+                className="w-full"
+              />
             </figure>
             <div className="card-body">
               <h2 className="card-title">{project.title}</h2>
               <p>{project.desc}</p>
+            </div>
+            <div className="absolute bottom-0 left-0 w-full h-0 bg-gradient-to-t from-[#0b0419cc] to-[#2d0366cc] flex items-center justify-center opacity-0 group-hover:h-full group-hover:opacity-90 transition-all duration-500">
+              <a className="text-white text-lg font-semibold tracking-wide hover:text-gray-400">
+                Watch Live
+              </a>
             </div>
           </motion.div>
         ))}
