c9bd39331bdef7df78f9fd9218a67545567f1a48
Sarkhail <sarkhailabbas@gmail.com>
2025-03-01 21:39:21 +0530

    Adjust the amount in the viewport.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -30,7 +30,7 @@ const ProjectsPage = () => {
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.5 }}
-            viewport={{ once: true, amount: 0.4 }}
+            viewport={{ once: true, amount: 0.2 }}
           >
             <figure>
               <img
