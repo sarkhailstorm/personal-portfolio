f55836c05c7b4d99bcdc47780d9b2c03666fe56d
Sarkhail <sarkhailabbas@gmail.com>
2025-03-01 16:27:02 +0530

    Fix image rendering issues.

--- src/data/details.tsx
+++ src/data/details.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 export const details: Record<string, React.ReactNode> = {
   internship: (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-2">
       <img
         src="https://www.apexure.com/images/icons/apexure-logo.svg"
         alt="Apexure Logo"
-        className="w-40"
+        width="180px"
+        height="auto"
       />
       <h1 className="font-bold text-xl">Web Developer Intern</h1>
       <p>
@@ -19,10 +20,15 @@ export const details: Record<string, React.ReactNode> = {
   ),
   experience: (
     <div className="flex flex-col gap-2">
-      <img src="/Coding.png" alt="Logo" className="w-10 h-10" />
+      <img src="/Coding.png" alt="Logo" width="48px" height="auto" />
       <h1 className="font-bold text-xl">Software Developer</h1>
       <div className="flex flex-col gap-2">
-        <p>Developed multiple full-stack web applications using modern technologies like Next.js, focusing on performance and user experience, while also actively contributing bug fixes and new features to open-source projects on GitHub.</p>
+        <p>
+          Developed multiple full-stack web applications using modern
+          technologies like Next.js, focusing on performance and user
+          experience, while also actively contributing bug fixes and new
+          features to open-source projects on GitHub.
+        </p>
       </div>
     </div>
   ),
@@ -31,11 +37,15 @@ export const details: Record<string, React.ReactNode> = {
       <img
         src="https://casetcollege.in/wp-content/uploads/2023/04/logonew.png"
         alt="Logo"
-        className="w-40"
+        width="180px"
+        height="auto"
       />
       <p className="text-xl font-bold">CS Graduate</p>
       <p>
-      Graduated from Caset College of Computer Sciences with great grades. Gained a solid foundation in software development, web technologies, and problem-solving through hands-on projects and coursework. Worked well in teams. 
+        Graduated from Caset College of Computer Sciences with great grades.
+        Gained a solid foundation in software development, web technologies, and
+        problem-solving through hands-on projects and coursework. Worked well in
+        teams.
       </p>
     </div>
   ),
