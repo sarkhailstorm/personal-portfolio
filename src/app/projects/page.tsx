3ad8bb5498cd5a252f4f6576f3a543633e48f985
Sarkhail <sarkhailabbas@gmail.com>
2025-03-01 12:08:01 +0530

    Create a separate file for projects data.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,53 +1,6 @@
 "use client";
+import { projects } from "@/data/projects";
 import { motion } from "framer-motion";
-
-const projects = [
-  {
-    img: "Portfolio.png",
-    title: "Portfolio Website",
-    desc: "An animated Portfolio Website built with Next.js, TypeScript, Tailwind CSS, Framer Motion, ShadCN, and DaisyUI.",
-  },
-  {
-    img: "Weather App.png",
-    title: "Weather App",
-    desc: "A simple Weather App designed with HTML, CSS, and JavaScript, featuring a visually appealing layout.",
-  },
-  {
-    img: "Task Management App.png",
-    title: "Task Management App",
-    desc: "A Task Management App with admin and employee dashboards, developed using React and Tailwind CSS.",
-  },
-  {
-    img: "3D Portfolio.png",
-    title: "3D Portfolio Website",
-    desc: "A 3D Portfolio Website crafted with React, Three.js, React-Three-Fiber, and Tailwind CSS, featuring interactive models.",
-  },
-  {
-    img: "Cloud File Manager.png",
-    title: "Cloud File Manager",
-    desc: "A simple app that uses Supabase Cloud for file storage with JWT authentication.",
-  },
-  {
-    img: "Google Calculator.png",
-    title: "Google Calculator",
-    desc: "A Google Calculator clone built with HTML, CSS, and JavaScript, replicating Google Calculator functionalities.",
-  },
-  {
-    img: "Coming Soon.png",
-    title: "Hang Tight!",
-    desc: "More projects are coming soon. You might wanna have a look at my github profile for more info.",
-  },
-  {
-    img: "Coming Soon.png",
-    title: "Hang Tight!",
-    desc: "More projects are coming soon. You might wanna have a look at my github profile for more info.",
-  },
-  {
-    img: "Coming Soon.png",
-    title: "Hang Tight!",
-    desc: "More projects are coming soon. You might wanna have a look at my github profile for more info.",
-  },
-];
 
 const ProjectsPage = () => {
   return (
