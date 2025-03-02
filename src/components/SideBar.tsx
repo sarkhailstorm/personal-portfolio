a374f46e6bb8b08e80f895b2938e8f56de2956f6
Sarkhail <sarkhailabbas@gmail.com>
2025-03-02 17:02:12 +0530

    Add the menu button and close button.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { IoHome } from "react-icons/io5";
 import { IoPerson } from "react-icons/io5";
 import { FaTools } from "react-icons/fa";
 import { FaFolderOpen } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa6";
 import { usePathname } from "next/navigation";
+import { AiOutlineMenuFold } from "react-icons/ai";
+import { IoCloseSharp } from "react-icons/io5";
 import { motion } from "framer-motion";
 
 const navLinks = [
@@ -40,32 +42,43 @@ const childVariants = {
 
 const SideBar = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <motion.nav
-      className="fixed right-0 flex flex-col gap-4 justify-center items-end h-screen p-4 z-50"
-      variants={containerVariants}
-      initial="hidden"
-      animate="show"
-    >
-      {navLinks.map(({ href, label, icon }) => (
-        <motion.div key={href} variants={childVariants}>
-          <Link
-            href={href}
-            className={`group relative flex items-center hover:gap-3 p-3 rounded-full transition-all duration-300 ease-in-out hover:pl-5 ${
-              pathname === href ? "bg-blue-700" : "bg-[#800020]"
-            }`}
-          >
-            <motion.span className="overflow-hidden opacity-0 w-0 group-hover:w-[4.4rem] group-hover:opacity-100 transition-all duration-300 ease-in-out text-white text-md">
-              {label}
-            </motion.span>
-            <motion.span className="text-white text-3xl transition-transform duration-300">
-              {icon}
-            </motion.span>
-          </Link>
-        </motion.div>
-      ))}
-    </motion.nav>
+    <>
+      <div
+        className="fixed top-5 right-5 z-50 p-3 cursor-pointer text-white text-3xl md:hidden"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? <IoCloseSharp /> : <AiOutlineMenuFold />}
+      </div>
+      <motion.nav
+        className={`fixed right-0 flex flex-col gap-4 justify-center items-end h-screen p-4 z-50 ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+        variants={containerVariants}
+        initial="hidden"
+        animate="show"
+      >
+        {navLinks.map(({ href, label, icon }) => (
+          <motion.div key={href} variants={childVariants}>
+            <Link
+              href={href}
+              className={`group relative flex items-center hover:gap-3 p-3 rounded-full transition-all duration-300 ease-in-out hover:pl-5 ${
+                pathname === href ? "bg-blue-700" : "bg-[#800020]"
+              }`}
+            >
+              <motion.span className="overflow-hidden opacity-0 w-0 group-hover:w-[4.4rem] group-hover:opacity-100 transition-all duration-300 ease-in-out text-white text-md">
+                {label}
+              </motion.span>
+              <motion.span className="text-white text-3xl transition-transform duration-300">
+                {icon}
+              </motion.span>
+            </Link>
+          </motion.div>
+        ))}
+      </motion.nav>
+    </>
   );
 };
 
