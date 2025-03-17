8f945c8f3537b785b3c3a441dd69ce9407000798
Sarkhail <sarkhailabbas@gmail.com>
2025-03-17 17:40:16 +0530

    Use next/image.

--- src/data/details.tsx
+++ src/data/details.tsx
@@ -1,13 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
 export const details: Record<string, React.ReactNode> = {
   internship: (
-    <div className="flex flex-col gap-2">
-      <img
-        src="https://www.apexure.com/images/icons/apexure-logo.svg"
+    <div className="flex flex-col gap-3">
+      <Image
+        src="/Apexure Logo.svg"
         alt="Apexure Logo"
-        width="180px"
-        height="auto"
+        width={150}
+        height={150}
+
       />
       <h1 className="font-bold text-xl">Web Developer Intern</h1>
       <p>
@@ -20,7 +22,7 @@ export const details: Record<string, React.ReactNode> = {
   ),
   experience: (
     <div className="flex flex-col gap-2">
-      <img src="/Coding.png" alt="Logo" width="48px" height="auto" />
+      <Image src="/Experience.png" alt="Logo" width={50} height={50} />
       <h1 className="font-bold text-xl">Software Developer</h1>
       <div className="flex flex-col gap-2">
         <p>
@@ -33,12 +35,13 @@ export const details: Record<string, React.ReactNode> = {
     </div>
   ),
   credentials: (
-    <div className="flex flex-col gap-2">
-      <img
-        src="https://casetcollege.in/wp-content/uploads/2023/04/logonew.png"
+    <div className="flex flex-col gap-3">
+      <Image
+        src="/Caset College Logo.png"
         alt="Logo"
-        width="180px"
-        height="auto"
+        width={150}
+        height={150}
+
       />
       <p className="text-xl font-bold">CS Graduate</p>
       <p>
