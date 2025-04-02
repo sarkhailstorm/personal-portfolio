cab4b502ac83efe2d54b6bfaa34fccccd17698e5
Sarkhail <sarkhailabbas@gmail.com>
2025-04-02 19:29:54 +0530

    Add a media query for small screen height devices.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ const HomePage = () => {
       animate="visible"
     >
       <motion.div
-        className="md:w-[43%] w-[80%] border-2 md:border-0 border-blue-500 mt-8 md:mt-0 
+        className="marginTop md:w-[43%] w-[80%] border-2 md:border-0 border-blue-500 mt-8 md:mt-0 
              md:rounded-none rounded-full overflow-hidden flex items-center justify-center md:cursor-auto cursor-pointer"
         variants={fadeInLeft}
         whileTap={screenSize === "mobile" ? { borderColor: "#ff004f", rotate: 360 } : {}}
