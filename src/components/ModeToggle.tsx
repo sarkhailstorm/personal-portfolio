59f6ebe997f94cdf5a0fdad5129576caf7b79fbd
Sarkhail <sarkhailabbas@gmail.com>
2025-05-03 22:34:24 +0530

    Feat(Dark Mode): Enable simple dark mode.

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -4,59 +4,36 @@ import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import toast from "react-hot-toast";
 
-type ModeToggleProps = {
-  onClick?: () => void;
-};
+export const ModeToggle: React.FC = () => {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
 
-export const ModeToggle: React.FC<ModeToggleProps> = ({ onClick }) => {
-  const { setTheme } = useTheme();
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          className="dark:text-white w-[39px] h-[39px] rounded-full"
-        >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem
-          onClick={() => {
-            toast("This feature will be available soon!", { icon: "ℹ️" });
-            if (onClick) onClick();
-          }}
-        >
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            setTheme("dark");
-            if (onClick) onClick();
-          }}
-        >
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            toast("This feature will be available soon!", { icon: "ℹ️" });
-            if (onClick) onClick();
-          }}
-        >
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={toggleTheme}
+      className="dark:text-white w-[39px] h-[39px] rounded-full"
+    >
+      {mounted ? (
+        theme === "dark" ? (
+          <Sun className="h-[1.2rem] w-[1.2rem]" />
+        ) : (
+          <Moon className="h-[1.2rem] w-[1.2rem]" />
+        )
+      ) : (
+        <div className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   );
 };
