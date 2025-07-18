a09d9daff5e17555f501a98b26aac7fcd18bb256
Sarkhail <sarkhailabbas@gmail.com>
2025-07-18 18:09:41 +0530

    Chore(Services Page): Improve card content.

--- src/data/services.ts
+++ src/data/services.ts
@@ -2,26 +2,26 @@ export const services = [
   {
     img: "Design.png",
     title: "Design",
-    desc: "I make things look awesome while keeping them easy to use. Good design isn't just about pretty colors—it's about making sure everything flows smoothly and feels right!",
+    desc: "I make things look awesome while keeping them easy to use. Good design isn't just about pretty colors,it's about making sure everything flows smoothly and feels right!",
   },
   {
     img: "Development.png",
     title: "Development",
-    desc: "From front-end magic to back-end logic, I bring ideas to life with clean and efficient code. Whether it’s a website, an app, or a full-stack project, I build things that just work.",
+    desc: "From front-end magic to back-end logic, I bring ideas to life with clean and efficient code. Whether it's a website, an app, or a full-stack project, I build things that just work.",
   },
   {
     img: "Problem Solving.png",
     title: "Problem Solving",
-    desc: "Got a tricky bug or a complex challenge? I love cracking tough problems and finding creative solutions that make everything run smoother and better.",
+    desc: "Got a weird bug or something acting up? I enjoy poking around until it makes sense again. I like keeping things secure too, no unexpected surprises on my watch.",
   },
   {
     img: "Security.png",
     title: "Security",
-    desc: "Keeping things safe and sound! I build applications with security in mind so there are no nasty surprises—just smooth, secure experiences for users.",
+    desc: "Keeping things safe and sound! I build applications with security in mind so there are no nasty surprises, just smooth, secure experiences for users.",
   },
   {
     img: "Efficiency.png",
     title: "Efficiency",
-    desc: "Fast, optimized, and hassle-free—that’s my style! I focus on making everything run as smoothly as possible, so users don’t have to deal with slow, clunky apps.",
+    desc: "Fast, optimized, and hassle-free, that's my style! I focus on making everything run as smoothly as possible, so users don't have to deal with slow, clunky apps.",
   },
 ];
